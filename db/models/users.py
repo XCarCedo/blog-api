6844f7ec01cb2f7a29f596b9ef98fabaf9de278c
Ahmad Maftoun <CarCedo.Pro@gmail.com>
2026-04-10 22:08:26 +0330

    fix: made username unique

--- db/models/users.py
+++ db/models/users.py
@@ -2,5 +2,5 @@ from sqlmodel import SQLModel, Field
 
 class User(SQLModel, table=True):
     id: int = Field(primary_key=True)
-    username: str = Field(index=True)
+    username: str = Field(index=True, unique=True)
     password_hash: str = Field()
