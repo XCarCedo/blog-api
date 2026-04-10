c8fabbd5f35b2be5ba9ba2b8bf128c07db1422fb
Ahmad Maftoun <CarCedo.Pro@gmail.com>
2026-04-10 23:20:28 +0330

    typo

--- db/database.py
+++ db/database.py
@@ -1,6 +1,6 @@
 from sqlmodel import create_engine, SQLModel
 
-sqlite_uri = "sqlite3:///./blog.sqlite3"
+sqlite_uri = "sqlite:///./blog.sqlite3"
 engine = create_engine(sqlite_uri, connect_args={"check_same_thread":False})
 
 def create_database_and_tables() -> None:
