650b55e08376ce49cb58b2074f1346efb8fb2efd
Ahmad Maftoun <CarCedo.Pro@gmail.com>
2026-04-10 23:18:38 +0330

    fix: fixed lifespan by making it a async contextmanager

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import Annotated
+from sqlmodel import select
+from contextlib import asynccontextmanager
 
+from hash import hash, verify
+from db.models.users import User
 from db.database import create_database_and_tables
-from schemas.users import Token
+from schemas.users import Token, UserSignup
 from dependencies.auth import UserDep, create_jwt_token
+from dependencies.database import SessionDep
 
+@asynccontextmanager
 async def lifespan(app: FastAPI):
     create_database_and_tables()
+    yield
 
 app = FastAPI(lifespan=lifespan)
 
-@app.post("/token")
-async def login_for_access_token(form: Annotated[OAuth2PasswordRequestForm, Depends()]):
-    pass
+
 
 @app.get("/me")
 async def get_me(user: UserDep):
