281256ab130223bb4f30b4989e57e774f420e747
Ahmad Maftoun <CarCedo.Pro@gmail.com>
2026-04-10 23:30:22 +0330

    refactor: moved all auth related endpoints to routers/auth.py

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
-from typing import Annotated
-from sqlmodel import select
+from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
-from hash import hash, verify
-from db.models.users import User
 from db.database import create_database_and_tables
-from schemas.users import Token, UserSignup
-from dependencies.auth import UserDep, create_jwt_token
-from dependencies.database import SessionDep
+from routers import auth
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -18,32 +11,4 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-@app.post("/token")
-async def login_for_access_token(form: Annotated[OAuth2PasswordRequestForm, Depends()], session: SessionDep):
-    user = session.exec(select(User).where(User.username==form.username)).first()
-    if not user:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, f"No such user with username of '{form.username}'")
-    if not verify(form.password, user.password_hash):
-        raise HTTPException(status.HTTP_401_UNAUTHORIZED, f"Incorrect password")
-    
-    payload = {
-        "id":user.id,
-        "username":form.username
-    }
-
-    return Token(access_token=create_jwt_token(payload), token_type="bearer")
-
-@app.post("/signup")
-async def signup(user: UserSignup, session: SessionDep):
-    username = user.username
-    hashed_password = hash(user.password)
-
-    new_user = User(username=username, password_hash=hashed_password)
-    session.add(new_user)
-    session.commit()
-
-    return {"ok":True}
-
-@app.get("/me")
-async def get_me(user: UserDep):
-    return user
+app.include_router(auth.router)
---
+++ routers/auth.py
@@ -0,0 +1,42 @@
+from fastapi import Depends, HTTPException, status, APIRouter
+from fastapi.security import OAuth2PasswordRequestForm
+from typing import Annotated
+from sqlmodel import select
+
+from hash import hash, verify
+from db.models.users import User
+from schemas.users import Token, UserSignup
+from dependencies.auth import UserDep, create_jwt_token
+from dependencies.database import SessionDep
+
+router = APIRouter()
+
+@router.post("/token")
+async def login_for_access_token(form: Annotated[OAuth2PasswordRequestForm, Depends()], session: SessionDep):
+    user = session.exec(select(User).where(User.username==form.username)).first()
+    if not user:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, f"No such user with username of '{form.username}'")
+    if not verify(form.password, user.password_hash):
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, f"Incorrect password")
+    
+    payload = {
+        "id":user.id,
+        "username":form.username
+    }
+
+    return Token(access_token=create_jwt_token(payload), token_type="bearer")
+
+@router.post("/signup")
+async def signup(user: UserSignup, session: SessionDep):
+    username = user.username
+    hashed_password = hash(user.password)
+
+    new_user = User(username=username, password_hash=hashed_password)
+    session.add(new_user)
+    session.commit()
+
+    return {"ok":True}
+
+@router.get("/me")
+async def get_me(user: UserDep):
+    return user
