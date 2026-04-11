35ce84d9b06bba65e7fc86c36403f87c5851ef5c
Ahmad Maftoun <CarCedo.Pro@gmail.com>
2026-04-11 19:04:27 +0330

    chore: included the blogs router

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from db.database import create_database_and_tables
-from routers import auth
+from routers import auth, blogs
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -12,3 +12,4 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth.router)
+app.include_router(blogs.router)
